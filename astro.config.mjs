808ae619304edcad3210ed5d49165e09d5f6610e
jhonatanmizu <natanjesuss20@gmail.com>
2026-02-28 23:40:03 -0300

    feat: improve meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.jhonatanmizu.com.br',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: "en",
